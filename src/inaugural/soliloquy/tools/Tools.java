e3c63edbab1818ce69a2f17a4f97e267587d6dc8
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2021-05-25 23:00:50 -0400

    Revising ImageAsset classes to permit both null and empty values, and to revert empty strings to null
    Revising ImageAsset classes to permit both null and empty values, and to revert empty strings to null

--- src/inaugural/soliloquy/tools/Tools.java
+++ src/inaugural/soliloquy/tools/Tools.java
@@ -4,4 +4,8 @@ public class Tools {
     public static String emptyIfNull(String string) {
         return string == null ? "" : string;
     }
+
+    public static String nullIfEmpty(String string) {
+        return "".equals(string) ? null : string;
+    }
 }
--- src/inaugural/soliloquy/tools/tests/ToolsTests.java
+++ src/inaugural/soliloquy/tools/tests/ToolsTests.java
@@ -4,6 +4,7 @@ import inaugural.soliloquy.tools.Tools;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class ToolsTests {
     @Test
@@ -13,5 +14,13 @@ class ToolsTests {
         assertEquals(input, Tools.emptyIfNull(input));
 
         assertEquals("", Tools.emptyIfNull(null));
+    }
+    @Test
+    void testNullIfEmpty() {
+        String input = "input";
+
+        assertEquals(input, Tools.emptyIfNull(input));
+
+        assertNull(Tools.emptyIfNull(""));
     }
 }
