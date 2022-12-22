ae25c5697062f1b3089ac878706b22347213655d
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2022-12-22 01:43:08 -0600

    refactor: Refactor Fake classes to Mocks in Common

--- src/main/java/inaugural/soliloquy/tools/testing/Mock.java
+++ src/main/java/inaugural/soliloquy/tools/testing/Mock.java
@@ -1,6 +1,12 @@
 package inaugural.soliloquy.tools.testing;
 
+import soliloquy.specs.common.persistence.PersistentValuesHandler;
 import soliloquy.specs.common.persistence.TypeHandler;
+import soliloquy.specs.common.valueobjects.Pair;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -8,6 +14,36 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class Mock {
+    public static <T> TypeHandler<T> generateSimpleMockTypeHandler(T[] values) {
+        //noinspection unchecked
+        TypeHandler<T> mockHandler = mock(TypeHandler.class);
+
+        for (T value : values) {
+            when(mockHandler.write(value)).thenReturn(value.toString());
+            when(mockHandler.read(value.toString())).thenReturn(value);
+        }
+
+        return mockHandler;
+    }
+
+    /** @noinspection rawtypes*/
+    public static Pair<PersistentValuesHandler, Map<String, TypeHandler>> generateMockPersistentValuesHandlerWithSimpleHandlers(Object[]... valueSets) {
+        PersistentValuesHandler mockPersistentValuesHandler = mock(PersistentValuesHandler.class);
+        HashMap<String, TypeHandler> handlers = new HashMap<>();
+
+        for (Object[] valueSet : valueSets) {
+            String type = valueSet[0].getClass().getCanonicalName();
+
+            TypeHandler typeHandler = generateSimpleMockTypeHandler(valueSet);
+
+            //noinspection unchecked
+            when(mockPersistentValuesHandler.getTypeHandler(type)).thenReturn(typeHandler);
+            handlers.put(type, typeHandler);
+        }
+
+        return Pair.of(mockPersistentValuesHandler, handlers);
+    }
+
     public static <T> TypeAndHandler<T> generateMockEntityAndHandler(Class<T> clazz,
                                                                      String writtenValue) {
         T mockEntity = mock(clazz);
--- src/test/java/inaugural/soliloquy/tools/tests/testing/MockTests.java
+++ src/test/java/inaugural/soliloquy/tools/tests/testing/MockTests.java
@@ -1,14 +1,63 @@
 package inaugural.soliloquy.tools.tests.testing;
 
+import inaugural.soliloquy.tools.random.Random;
 import inaugural.soliloquy.tools.testing.Mock;
 import org.junit.jupiter.api.Test;
+import soliloquy.specs.common.persistence.PersistentValuesHandler;
 import soliloquy.specs.common.persistence.TypeHandler;
+import soliloquy.specs.common.valueobjects.Pair;
 import soliloquy.specs.gamestate.entities.Item;
 
-import static inaugural.soliloquy.tools.testing.Mock.generateMockEntityAndHandler;
+import java.util.Map;
+
+import static inaugural.soliloquy.tools.testing.Mock.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MockTests {
+    @Test
+    void testGenerateSimpleMockTypeHandler() {
+        Integer[] values =
+                new Integer[]{Random.randomInt(), Random.randomInt(), Random.randomInt()};
+
+        TypeHandler<Integer> mockIntegerHandler = generateSimpleMockTypeHandler(values);
+
+        assertNotNull(mockIntegerHandler);
+        for (Integer value : values) {
+            assertEquals(value, mockIntegerHandler.read(value.toString()));
+            assertEquals(value.toString(), mockIntegerHandler.write(value));
+        }
+    }
+
+    @Test
+    void testGenerateMockPersistentValuesHandlerWithSimpleHandlers() {
+        Integer[] ints =
+                new Integer[]{Random.randomInt(), Random.randomInt(), Random.randomInt()};
+        Double[] doubles =
+                new Double[]{Random.randomDouble(), Random.randomDouble(), Random.randomDouble()};
+
+        //noinspection rawtypes
+        Pair<PersistentValuesHandler, Map<String, TypeHandler>> persistentValuesHandlerAndHandlers = generateMockPersistentValuesHandlerWithSimpleHandlers(ints, doubles);
+
+        assertNotNull(persistentValuesHandlerAndHandlers);
+        assertNotNull(persistentValuesHandlerAndHandlers.getItem1());
+        assertNotNull(persistentValuesHandlerAndHandlers.getItem2());
+        //noinspection rawtypes
+        TypeHandler integerHandler = persistentValuesHandlerAndHandlers.getItem2().get(Integer.class.getCanonicalName());
+        for (Integer value : ints) {
+            assertEquals(value, integerHandler.read(value.toString()));
+            //noinspection unchecked
+            assertEquals(value.toString(), integerHandler.write(value));
+        }
+        //noinspection rawtypes
+        TypeHandler doubleHandler = persistentValuesHandlerAndHandlers.getItem2().get(Double.class.getCanonicalName());
+        for (Double value : doubles) {
+            assertEquals(value, doubleHandler.read(value.toString()));
+            //noinspection unchecked
+            assertEquals(value.toString(), doubleHandler.write(value));
+        }
+
+    }
+
     @Test
     void testGenerateMockEntityAndHandler() {
         String writtenValue = "writtenValue";
